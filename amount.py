7392deb0bae872c9045278b5851ae541c9dbec9a
yilkimezakka@gmail.com <yilkimezakka@gmail.com>
2025-07-16 20:26:11 +0000

    first commit

---
+++ amount.py
@@ -0,0 +1,25 @@
+# class work
+
+allowance = 2000
+allowance -=400
+print(allowance)
+
+allowance += 100
+print(allowance)
+
+allowance -= 250
+print(allowance)
+
+percent = 25 / 100
+percent = allowance * 25 / 100
+allowance -= percent
+print(f"Allowance: {allowance}")
+
+third = allowance / 3
+allowance -= third
+print(allowance)
+
+allowance //=2
+print(allowance)
+allowance %=100
+print(allowance)
--- logical.py
+++ logical.py
@@ -1,4 +1,6 @@
+
 # A simple calculator app
+"""
 print('''********************
 1. Addition
 2. Subtraction
@@ -7,7 +9,6 @@ print('''********************
 5. Exponential
 6. Floor division
 **************************''')
-
 # ADDITION
 print("Enter two numbers to add")
 first_number = float(input("first Number: "))
@@ -49,4 +50,97 @@ first_number = float(input("first Number: "))
 second_number = float(input("second Number: "))
 sum = first_number // second_number
 print(f"{first_number:.2f} // {second_number:.2f}: {sum:.2f}")
+"""
+"""
+first_number = 5
+first_number += 10
+print(first_number)
+
+first_number = 25
+first_number -= 10
+print(first_number)
+
+first_number = 5
+first_number *= 10
+print(first_number)
+
+first_number = 50
+first_number /= 10
+print(first_number)
+
+first_number = 5
+first_number **= 3
+print(first_number)
+
+first_number = 50
+first_number //= 10
+print(first_number)
+"""
+
+weekly_allowance = 2000
+
+book_spent = 400
+
+allowance = weekly_allowance - book_spent
+
+amount_found = 100
+
+amount_added = allowance + amount_found
+
+amount_for_snacks = 250
+
+remaining_balance = amount_added - amount_for_snacks
+
+percentage = 25/100
+
+percent_balance = remaining_balance - percentage
+
+data_allowance = 1/3
+
+amount_left = percent_balance * data_allowance
+
+savings = 2
+
+equal_spent = amount_left / savings
+
+total_allowance = 100
+
+final_allowance = equal_spent % total_allowance
+
+print(allowance)
+print(amount_added)
+print(remaining_balance)
+print(percent_balance)
+print(amount_left)
+print(equal_spent)
+print(final_allowance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
